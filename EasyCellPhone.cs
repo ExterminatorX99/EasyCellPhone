81d44916aaf22f99cd28be5a15568b5a33742d5f
Kristijan Kos <kkos1@tvz.hr>
2021-06-25 14:32:50 +0200

    Code

---
+++ EasyCellPhone.cs
@@ -0,0 +1,8 @@
+using Terraria.ModLoader;
+
+namespace EasyCellPhone
+{
+    public class EasyCellPhone : Mod
+    {
+    }
+}
---
+++ EasyNPC.cs
@@ -0,0 +1,32 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace EasyCellPhone
+{
+    public class EasyNPC : GlobalNPC
+    {
+        private static readonly short[] MechanicExtraItems =
+        {
+            ItemID.MagicMirror,
+            ItemID.DepthMeter,
+            ItemID.Compass,
+            ItemID.Radar,
+            ItemID.TallyCounter,
+            ItemID.LifeformAnalyzer,
+            ItemID.DPSMeter,
+            ItemID.Stopwatch,
+            ItemID.MetalDetector,
+            ItemID.FishermansGuide,
+            ItemID.WeatherRadio,
+            ItemID.Sextant
+        };
+
+        public override void SetupShop(int type, Chest shop, ref int nextSlot)
+        {
+            if (type == NPCID.Mechanic)
+                foreach (short item in MechanicExtraItems)
+                    shop.item[nextSlot++].SetDefaults(item);
+        }
+    }
+}
